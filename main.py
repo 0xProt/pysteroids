f96c38fafe6a6d372bdbc8d859a9b62ff6e9220c
0xProt <prottyweep@proton.me>
2025-01-26 23:13:55 +0000

    circle and player shape classes added

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
 import constants
+from player import Player
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
 
 
 def main():
-    print("Starting asteroids!")
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
