a007e066e4f756674132c9133f5c6b2bae96778a
0xProt <prottyweep@proton.me>
2025-01-27 10:28:36 +0000

    added: asteroids, field, shooting, destruction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 import constants
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from player import Player
+from shot import Shot
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 
@@ -9,6 +12,18 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     dt = 0
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
@@ -16,9 +31,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updatable:
+            obj.update(dt)
+        for asteroid in asteroids:
+            if asteroid.is_colliding(player):
+                print("Game Over !")
+                return
+            for bullet in shots:
+                if asteroid.is_colliding(bullet):
+                    asteroid.kill()
+                    bullet.kill()
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 
